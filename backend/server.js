c81f13bcf236c04f7bb0135d3c9cb10b7e8d18c0
liuyuelintop <liuyuelintop@gmail.com>
2025-03-06 16:37:54 +1100

    Coupon Routes Completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
