f5b7cb922dcb1af382c1a6db8adb1c3d8fadcbdc
liuyuelintop <liuyuelintop@gmail.com>
2025-03-10 15:02:49 +1100

    （feat): Admin Dash Board Completed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json()); // allows you to parse the body of request
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
+import AdminPage from "./pages/AdminPage"
 import Navbar from "./components/Navbar"
 import { Toaster } from "react-hot-toast"
 import { useUserStore } from "./stores/useUserStore"
@@ -31,6 +32,10 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
           <Route path="/signup" element={!user ? <SignUpPage /> : <Navbar to="/" />} />
+          <Route
+            path='/secret-dashboard'
+            element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}
+          />
         </Routes>
       </div>
       <Toaster />
