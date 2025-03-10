7a4850a8e26f5be8435732b6b73772edc8f50477
liuyuelintop <liuyuelintop@gmail.com>
2025-03-10 20:55:05 +1100

    fix: `__dirname` resolve in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of request
 app.use(cookieParser());
 
